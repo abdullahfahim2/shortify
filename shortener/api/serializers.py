5fe2aef0761c1482847ecce3f39020d1a956464a
abdullahfahim2 <abdullahfahim680578@gmail.com>
2025-11-01 22:43:07 +0600

    add : serializers for shortlink models api

--- shortener/api/serializers.py
+++ shortener/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from core.models import AnonymousShortURL
+from core.models import AnonymousShortURL , UserLink , ClickEvent
 
 
 class Anonnymousshorturlserializer(serializers.ModelSerializer):
     class Meta:
         model = AnonymousShortURL
         fields = '__all__'
+
+class Clickeventserializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClickEvent
+        fields = '__all__'
+
+
+class UserlinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserLink
+        fields = '__all__'
+
+        
