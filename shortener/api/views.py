bd9c024cc4a00be3417661421fe79da0dddcfb66
abdullahfahim2 <abdullahfahim680578@gmail.com>
2025-11-03 23:07:42 +0600

    fix: import serializers

--- shortener/api/views.py
+++ shortener/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .serializers import *
+from .serializers import Anonnymousshorturlserializer , Clickeventserializer , UserlinkSerializer
 from core.models import AnonymousShortURL
 
 
